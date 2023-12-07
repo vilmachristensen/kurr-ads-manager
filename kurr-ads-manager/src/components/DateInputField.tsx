c68204c52463f854261d4c74777dadd37ba31387
id19lns <id19lns@cs.umu.se>
2023-12-07 17:36:28 +0100

    added dateInputField

--- kurr-ads-manager/src/components/DateInputField.tsx
+++ kurr-ads-manager/src/components/DateInputField.tsx
@@ -3,20 +3,23 @@ import styled from 'styled-components';
 import {Default_medium} from '../styles/Text';
 import Colors from '../styles/Colors';
 import TextField from '@mui/material/TextField';
+import { DatePicker } from '@mui/x-date-pickers';
 
 interface DateInputFieldProps {
-  label?: string;
-  disabled: boolean;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  value: string | number;
-  required?: boolean;
+    value: formCampaign.endDate.toISOString().split('T')[0]
 }
 
-const DateInputField: React.FC<DateInputFieldProps> = ({ label, disabled, onChange, value, required }) => {
+const DateInputField: React.FC<DateInputFieldProps> = ({  }) => {
+    const [selectedDate, setSelectedDate] = React.useState(new Date());
+
     return (
-        disabled
-          ? <TextField style={{width: "inherit"}} disabled id="outlined-basic" label={label} variant="outlined" InputLabelProps={{ shrink: true }} required={required}  onChange={(e) => onChange(e as React.ChangeEvent<HTMLInputElement>)} />
-          : <TextField style={{width: "inherit"}}id="outlined-basic" label={label} variant="outlined" InputLabelProps={{ shrink: true }} required={required}  onChange={(e) => onChange(e as React.ChangeEvent<HTMLInputElement>)}/>
+        <DatePicker
+        label="none"
+        value={value}
+        onChange={(e) =>
+          setFormCampaign({ ...formCampaign, endDate: new Date(e.target.value) })
+        }
+      />
       );
 };
 
